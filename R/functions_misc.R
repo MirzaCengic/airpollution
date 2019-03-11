34fb265e963afd5755b416f57fef3ecc80e883aa
mirzacengic <mirzaceng@gmail.com>
2019-03-11 01:27:43 +0100

    Helper functions

---
+++ R/functions_misc.R
@@ -0,0 +1,65 @@
+
+#' Convert to rayshader object
+#' 
+#' Takes in raster and convert to rayshader friendly object
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rayshaderize <- function(x)
+{
+  xx <- matrix(
+    raster::extract(x,raster::extent(x)),
+    nrow = ncol(x),
+    ncol = nrow(x))
+  return(xx)
+}
+
+
+#' Flip for plotting
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fliplr = function(x) {
+  # xx <- getValues(x)
+  x[,ncol(x):1]
+}
+
+#' Prepare overlay data
+#' 
+#' take simple feature (from osm), and raster, and make layer for add_overlay from rayshader
+#'
+#' @param poly 
+#' @param raster 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+prepare_overlay_data <- function(poly, raster, color = "black")
+{
+  # if (inhe)
+  
+  data_r <- raster::rasterize(as(poly, "Spatial"), raster, 1, background=0)
+  
+  
+  mat_r <- matrix(
+    raster::extract(data_r,raster::extent(data_r)),
+    nrow = ncol(data_r),
+    ncol = nrow(data_r))
+  
+  tempfilename = tempfile()
+  png(tempfilename,width = nrow(mat_r), height = ncol(mat_r))
+  par(mar = c(0,0,0,0))
+  raster::image(fliplr(mat_r), axes = FALSE, col = c("transparent", color))
+  dev.off()
+  map_r <- png::readPNG(tempfilename)
+  return(map_r)
+}
